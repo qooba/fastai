1a7445aad47b737e35f97fcaf8bc84d7c7f3416c
qooba <dev@qooba.net>
2020-04-13 21:25:52 +0200

    change hpc

--- hpc/run.sh
+++ hpc/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-docker run --gpus all  --name jupyter -d --rm -p 8888:8888 -v /home/qba/Qooba/Docker/fastai/_fastai:/root/.fastai -v /home/qba/Qooba/Docker/fastai:/opt/notebooks qooba/fastai:20.01-py3 /bin/bash -c "jupyter lab --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root --NotebookApp.password='123QWEasd' --NotebookApp.token='123QWEasd'"
+docker run --gpus all  --name jupyter -d --rm -p 8888:8888 -v /home/qba/Qooba/Docker/fastai/_fastai:/root/.fastai -v /home/qba/Qooba/Docker/fastai:/opt/notebooks qooba/fastai:hpc /bin/bash -c "jupyter lab --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root --NotebookApp.password='123QWEasd' --NotebookApp.token='123QWEasd'"
 #docker run --gpus all --shm-size 8G --name jupyter -d --rm -p 8888:8888 -v /home/qba/Qooba/Docker/fastai:/opt/notebooks qooba/anaconda3:fastai /bin/bash -c "jupyter lab --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root --NotebookApp.password='123QWEasd' --NotebookApp.token='123QWEasd'"
